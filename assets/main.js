1984816b251e346541410f3db9de29b42459b558
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-23 08:40:32 -0300

    feat Js logic finished

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
-const panels = document.querySelectorAll(".panels");
+// pega todos os items com a classe panel
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+    this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+    if (e.propertyName.includes("flex")) {
+        this.classList.toggle("open-active");
+    }
+}
+
+// Cada um dos panels vão ter um evento click
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) => panel.addEventListener("transitioned", toggleActive));
